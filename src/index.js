64efee75464e4ecc250e94f425df89036a30696e
Christopher Girvin <bugbounty.chrisg8691@gmail.com>
2020-10-29 00:34:37 -0400

    basic functionality/redux hooked up

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-import 'bulma/css/bulma.css';
-import './styles.scss';
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+import reducer from "./reducers";
+
+import "bulma/css/bulma.css";
+import "./styles.scss";
+
+const store = createStore(reducer);
+
+const rootElement = document.getElementById("root");
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
